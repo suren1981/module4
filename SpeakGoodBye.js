bb5941ce452ff9b7e20bf05bc97775bb2740f29c
suren1981 <suren.hakobjanyan@gmail.com>
2016-11-21 18:23:38 +0400

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -9,7 +9,7 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 (function (window) {
-var byeSpeaker = {}
+var byeSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
